c2ed3f74a1b895e1e4f9070bf9c53519d2aff654
Johanz9 <johanz147@gmail.com>
2020-10-13 13:18:11 +0200

    init git

---
+++ Code/trafficLight.cpp
@@ -0,0 +1,106 @@
+///
+/// NOTE On Desktop systems, compile with -D NO_PI flag
+//
+
+#define NO_PI
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <iostream>
+
+#ifndef NO_PI
+#include <wiringPi.h>
+#endif
+
+using namespace std;
+
+const int led_Red = 0;
+const int led_Green = 1;
+const int led_Yellow = 2;
+const int timeoutMs = 1000; // 1 second
+
+///
+/// Utils
+///
+void init()
+{
+#ifndef NO_PI
+    wiringPiSetup();
+    pinMode(led_Blue, OUTPUT);
+#endif
+}
+
+void setLed(int ledNumber, bool value)
+{
+#ifndef NO_PI
+    digitalWrite(ledNumber, value);
+#else
+    cout << "Setting led " << ledNumber << " to " << (value ? "ON" : "OFF") << endl;
+#endif
+}
+
+void counter(int time, int led){
+    int count = 0;
+
+    if(led == led_Yellow){
+        while(count < time){
+            //*cout << " N " << time <<endl;
+            setLed(led, true);
+            setLed(led_Green,true);
+            count++;
+        }
+        setLed(led,false);
+        setLed(led_Green,false);
+
+    #ifndef NO_PI
+        delay(timeoutMs);
+    #else
+        Sleep(timeoutMs / 1000);
+    #endif
+
+
+    } else {
+
+        while (count < time) {
+            //*cout << " N " << time <<endl;
+            setLed(led, true);
+            count++;
+        }
+        setLed(led, false);
+    #ifndef NO_PI
+            delay(timeoutMs);
+    #else
+            Sleep(timeoutMs / 1000);
+    #endif
+    }
+}
+
+void normalCycle(){
+
+    //int count = 0;
+    int time1 = 100;
+    int time2 = 100;
+    int time3 = 50;
+    while(1)
+    {
+        counter(time1,led_Red);
+        counter(time2,led_Green);
+        counter(time3,led_Yellow);
+
+    }
+}
+
+
+int main()
+{
+    init();
+    normalCycle();
+
+    return 0;
+
+
+}
